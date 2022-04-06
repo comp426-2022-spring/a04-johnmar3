1bf2dbe2e7c934bd5f6be00527ed2f3b21b4f5cb
@johnmar3 <johnmar3@ad.unc.edu>
2022-04-06 17:30:19 -0400

    update fix

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.use((req, res, next) => {
     console.log(logdata)
     const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referrer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
     const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referrer, logdata.useragent)
-    console.log(info)
+    //console.log(info)
     next();
 })
 
